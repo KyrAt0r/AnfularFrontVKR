03ff16669b0b422466b96b68d4cde1d05f4971f2
KyrAt0r <49461100+KyrAt0r@users.noreply.github.com>
2020-04-17 01:09:09 +0500

    Tray API
    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router';
 
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { MainPageComponent } from './MainPage/MainPage.component';
-import { LoginPageComponent } from './login-page/login-page.component'
+import { ECPComponent } from './edit-content/edit-content.components'
+import { NewServiceService } from './new-service.service';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'main', component: MainPageComponent},
+  {path: 'EditContent', component:ECPComponent}
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NewServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
