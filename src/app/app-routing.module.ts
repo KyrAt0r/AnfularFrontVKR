0c86f20b1570350c4a638f993c3ca1874565a275
KyrAt0r <49461100+KyrAt0r@users.noreply.github.com>
2020-04-16 22:07:31 +0500

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './MainPage/MainPage.component';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {path: 'login', component: LoginPageComponent},
+  {path: 'main', component: MainPageComponent}
 ]
 
 @NgModule({
